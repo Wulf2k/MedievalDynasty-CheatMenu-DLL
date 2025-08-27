25c3def79fcd80f4608305cd6452fdcb5f176d8a
Wulf2k <wulf2k@gmail.com>
2025-08-26 19:27:52 -0500

    fixed minor code error

--- MedievalDynasty-DLL/Project.cpp
+++ MedievalDynasty-DLL/Project.cpp
@@ -211,7 +211,7 @@ DLLEXPORT void __cdecl initialStuff(void*)
 	signature = ScanData("48 89 5C 24 10 48 89 74 24 18 55 57 41 54 41 56 41 57 48 8D AC 24 50 FF FF FF 48 81 EC B0 01 00 00 48 8B ?? ?? ?? ?? ?? 48 33 C4 48 89 85 A8 00 00 00");
 	data = ScanData(0x2000000);
 	memcpy(data.data, hMD, data.size);
-	uintptr_t offset = bruteForce(signature, data);
+	offset = bruteForce(signature, data);
 	MDGameFunctions.StaticConstructObject = ((DWORD64)hMD + offset);
 	*(PDWORD64)&StaticConstructObject = MDGameFunctions.StaticConstructObject;
 
